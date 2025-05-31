2eb96b9ebb622b168cd510b4b5c3fd9c5cf2d35b
Kat Berton <katherine.berton@gmail.com>
2025-05-30 17:59:00 -0700

    stronger class presence

--- project_add_monk.py
+++ project_add_monk.py
@@ -16,7 +16,12 @@ class Gameplay:
   def __init__(self):
     self.strahd_initiative = random.randint(1,20)
 
-  def get_player_initiative(self):
+  def set_player(self):
+    player_name = ask_for_name().title()
+    classes.print()
+    self.player = ask_for_class(player_name)
+
+  def set_player_initiative(self):
     self.player_initiative = initiative_roll(competing_initiative=self.strahd_initiative)
 
     # If player goes first, player goes on even turns, or turns where turn_count % 2 == 0.
@@ -33,43 +38,33 @@ class Gameplay:
     ascii_art.print()
     intro_monologue.play()
 
-
-
-#------------------------------------INTRO AND SETUP----------------------------------------
 game = Gameplay()
-
-game.intro()
-
-player_name = ask_for_name().title()
-classes.print()
-player = ask_for_class(player_name)
-
-conceit.print()
-
-game.get_player_initiative()
 strahd = Strahd(name="Strahd")
 
+game.intro()
+game.set_player()
+conceit.print()
+game.set_player_initiative()
 
 #------------------------------GAMEPLAY/TURN ALTERNATOR-------------------------------------
 
-
-while player.current_hp > 0 and strahd.current_hp > 0: #While both Strahd and player are up and fighting
+while game.player.current_hp > 0 and strahd.current_hp > 0: #While both Strahd and player are up and fighting
   # If it's currently player's turn:
   if game.is_players_turn():
     while True: #loop to capture invalid choices (miskeys or attempts to SPECIAL too many times)
       player_move = input("What would you like to do? ATTACK or SPECIAL? ")
 
       if player_move.lower() == "attack":
-        strahd.take_damage(player.attack_action(competing_ac=strahd.armor_class))
+        strahd.take_damage(game.player.attack_action(competing_ac=strahd.armor_class))
         strahd_status(current_hp=strahd.current_hp, max_hp=strahd.max_hp)
         print()
         break
 
       elif player_move.lower() == "special":
-        if player.current_specials <= 0:
+        if game.player.current_specials <= 0:
           print("No specials left!")
         else:
-          results = player.special(competing_ac=strahd.armor_class)
+          results = game.player.special(competing_ac=strahd.armor_class)
           strahd.take_damage(results.damage)
           strahd.stunned = results.stunned
           break
@@ -80,15 +75,15 @@ while player.current_hp > 0 and strahd.current_hp > 0: #While both Strahd and pl
   # If it's currently Strahd's turn
   else:
     if strahd.stunned == False:
-        player.take_damage(strahd.attack(competing_ac=player.armor_class))
+        game.player.take_damage(strahd.attack(competing_ac=game.player.armor_class))
     else:
         print("Strahd is stunned! He strains to lunge at you but he's too stiff to move.")
         print()
         strahd.stunned = False # Remove stunned status after one turn
 
-    if player.current_hp > 0:
-      print(f"Your remaining hp: {player.current_hp}.")
+    if game.player.current_hp > 0:
+      print(f"Your remaining hp: {game.player.current_hp}.")
 
   game.increment_turn()
 
-ending_sequence(player=player, last_move=strahd.last_move)
+ending_sequence(player=game.player, last_move=strahd.last_move)
